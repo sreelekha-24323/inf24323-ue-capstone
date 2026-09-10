e7bb126e834b3299d75215ba4f297e426ec02c8f
sreelekha-24323 <inf24323@adobe.com>
2026-09-11 00:39:56 +0530

    added some style changes for custom button block

--- blocks/custombutton/custombutton.js
+++ blocks/custombutton/custombutton.js
@@ -1,10 +1,12 @@
 export default function decorate(block) {
-  console.log("block",block)
   const link = block.querySelector('a[href]');
   if (!link) {
     block.textContent = '';
     return;
   }
+
+  const type = block.children[3]?.textContent.trim();
   link.classList.add('button');
+  if (type) link.classList.add(type);
 }
  
