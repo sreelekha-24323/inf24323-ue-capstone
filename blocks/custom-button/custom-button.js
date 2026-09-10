0ebdc3183ccac2fef5b91c5824efd8a6342a58e2
sreelekha-24323 <inf24323@adobe.com>
2026-09-11 01:22:41 +0530

    added css styling

--- blocks/custom-button/custom-button.js
+++ blocks/custom-button/custom-button.js
@@ -1,13 +1,6 @@
 export default function decorate(block) {
   const link = block.querySelector('a[href]');
   console.log("link",link)
-  // if (!link) {
-  //   block.textContent = '';
-  //   return;
-  // }
-
-  // const type = block.children[3]?.textContent.trim();
   link.classList.add('button');
-  // if (type) link.classList.add(type);
 }
  
